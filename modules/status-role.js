ed5f28efc56c195c23be10b77e3fc9d1e908458f
Zedro2742 <61875966+Zedro2742@users.noreply.github.com>
2022-01-08 19:50:59 +0530

    Add files via upload

---
+++ modules/status-role.js
@@ -0,0 +1,75 @@
+const Discord = require("discord.js");
+const config = require("../botconfig/config.json");
+
+module.exports = client => {
+
+    const description = {
+        name: "Status Role",
+    }
+    client.logger(`〢 Module: Loaded ${description.name}`.bold.green);
+
+    client.on("presenceUpdate", async (oP, nP) => {
+        let guild = client.guilds.cache.get(config.serverId)
+        if (!guild) return;
+        if (nP) {
+
+            var user = guild.members.cache.get(nP.userId);
+
+            if (!user || !user.roles) user = await guild.members.fetch(nP.userId).catch(() => {}) || false;
+
+            if (!user) return;
+
+            if (nP.activities.some(({
+                    state
+                }) => state?.includes(config.status))) {
+                if (!user.roles.cache.has(config.roleId)) {
+                    user.roles.add(config.roleId).catch(() => {});
+                }
+            } else {
+                if (user.roles.cache.has(config.roleId)) {
+                    user.roles.remove(config.roleId).catch(() => {});
+                }
+            }
+        }
+    })
+
+    client.on("ready", async () => {
+
+        let guild = client.guilds.cache.get(config.serverId)
+        if (!guild) return;
+
+        let fm = await guild.members.fetch().catch(() => {})
+
+        let members_who_need = [...fm.filter(user =>
+            !user.user.bot && !user.roles.cache.has(config.roleId) &&
+            user.presence && user.presence.activities.some(({
+                state
+            }) => state?.includes(config.status))
+        ).values()];
+
+        let noStatus = [...fm.filter(user =>
+            !user.user.bot && !user.roles.cache.has(config.roleId) &&
+            (!user.presence || !user.presence.activities.some(({
+                state
+            }) => state?.includes(config.status)))
+        ).values()];
+
+        for (const user of members_who_need) {
+            await user.roles.add(config.roleId).catch(() => {});
+            await delay(350);
+        }
+
+        for (const user of noStatus) {
+            await user.roles.remove(config.roleId).catch(() => {});
+            await delay(350);
+        }
+    })
+
+    function delay(delayInms) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(2);
+            }, delayInms);
+        });
+    }
+}
